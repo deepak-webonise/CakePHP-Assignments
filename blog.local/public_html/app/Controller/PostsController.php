c5f0ec2edf6fa7438a4639625e49a6cc02e7b6e1
deepak.webonise <deepak.kabbur@weboniselab.com>
2014-10-31 18:16:04 +0530

    Updated Post model and PostController

--- blog.local/public_html/app/Controller/PostsController.php
+++ blog.local/public_html/app/Controller/PostsController.php
@@ -3,41 +3,42 @@
 class PostsController extends AppController {
 	public $helpers = array('Html', 'Form');
 	public function index() {
-		$this->set('posts', $this->Post->find('all'));
+		$this->set('posts', $this->Post->findPosts());
 	}
+
 	/* To view Post*/
-	public function view($id = null) {
-		if(!$id) {
-			throw new NotFoundException('Invalid post');
-		}
-		$post = $this->Post->findById($id);
-		if (!$post) {
-			throw new NotFoundException('Invalid post');
-		}
-		$this->set('post', $post);
+    /**
+     * @param null $id
+     * @throws NotFoundException
+     */
+    public function view($id = null) {
+		$this->set('post', $this->Post->findByPostId($id));
 	}
+
 	/*To add Post*/
-	public function add() {
+    /**
+     * @description:
+     */
+    public function add() {
         if ($this->request->is('post')) {
-            $this->Post->create();
-            if ($this->Post->save($this->request->data)) {
+
+            if ($this->Post->addPost($this->request->data)) {
                 $this->Session->setFlash(__('Your post has been saved.'));
                 return $this->redirect(array('action' => 'index'));
             }
             $this->Session->setFlash(__('Unable to add your post.'));
         }
     }
+
+    /**
+     * @param null $id
+     */
+
     public function edit($id = null) {
-		if (!$id) {
-			throw new NotFoundException(__('Invalid post'));
-		}
-		$post = $this->Post->findById($id);
-		if (!$post) {
-			throw new NotFoundException(__('Invalid post'));
-		}
+		$post = $this->Post->findByPostId($id);
+
 		if ($this->request->is(array('post', 'put'))) {
-			$this->Post->id = $id;
-			if ($this->Post->save($this->request->data)) {
+			if ($this->Post->editPost($this->request->data, $id)) {
 				$this->Session->setFlash(__('Your post has been updated.'));
 				return $this->redirect(array('action' => 'index'));
 			}
@@ -47,12 +48,16 @@ class PostsController extends AppController {
 			$this->request->data = $post;
 		}
 	}
-	/*Delete Post*/
-	public function delete($id) {
+
+    /**
+     * @param $id
+     * @throws MethodNotAllowedException
+     */
+    public function delete($id) {
 		if ($this->request->is('get')) {
 			throw new MethodNotAllowedException();
 		}
-		if ($this->Post->delete($id)) {
+		if ($this->Post->deletePost($id)) {
 			$this->Session->setFlash(__('The post with id: %s has been deleted.', h($id)));
 			return $this->redirect(array('action' => 'index'));
 		}
--- blog.local/public_html/app/Model/Post.php
+++ blog.local/public_html/app/Model/Post.php
@@ -11,5 +11,81 @@ class Post extends AppModel
 		)
 	);
 
+    /**
+     * @return array
+     */
+
+    public function findPosts(){
+
+       return $this->find('all');
+
+    }
+
+    /**
+     * @param null $id
+     * @return array
+     * @throws NotFoundException
+     */
+    public function findByPostId($id=null)
+    {
+        if(!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        return $this->find('first', array('conditions'=> array('Post.id' => $id)));
+    }
+
+    /**
+     * @param null $data
+     * @return bool
+     */
+    public function addPost($data = null)
+    {
+       if(!$data){
+           $this->create();
+           if ($this->save($data)) {
+               return true;
+           }
+       }
+
+       return false;
+    }
+
+    /**
+     * @param null $data
+     * @param null $id
+     * @return bool
+     * @throws NotFoundException
+     */
+    public function editPost($data=null,$id=null){
+        if(!$id)
+        {
+            throw new NotFoundException(__('Invalid post'));
+        }
+        $this->set('id',$id);
+        if($data){
+            if ($this->save($data)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $id
+     * @return bool
+     * @throws NotFoundException
+     */
+    public function deletePost($id)
+    {
+        if(!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+        if($this->delete($id)){
+            return true;
+        }
+        return false;
+    }
 }
 ?>
